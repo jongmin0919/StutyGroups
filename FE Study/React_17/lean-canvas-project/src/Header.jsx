351633166171a2715ee934cd2c3a4b1eaeb49f6d
Re_Go <jongmin0919@naver.com>
2024-09-30 23:55:05 +0900

    header added

--- FE Study/React_17/lean-canvas-project/src/Header.jsx
+++ FE Study/React_17/lean-canvas-project/src/Header.jsx
@@ -1,17 +1,71 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import {
+  FaHome,
+  FaInfoCircle,
+  FaEnvelope,
+  FaBars,
+  FaTimes,
+} from 'react-icons/fa';
 
 function Header() {
-  const navigate = useNavigate();
+  const navItems = [
+    { id: 'home', label: 'Home', icon: <FaHome />, to: '/' },
+    { id: 'about', label: 'About', icon: <FaInfoCircle />, to: '/about' },
+    { id: 'contact', label: 'Contact', icon: <FaEnvelope />, to: '/contact' },
+  ];
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <div>
-      <ul>
-        <li onClick={() => navigate('/')}>Home</li>
-        <li onClick={() => navigate('/about')}>About</li>
-        <li onClick={() => navigate('/contact')}>Contact</li>
-      </ul>
-    </div>
+    <header className="sticky top-0 bg-gray-800 text-white px-4">
+      <div className="container mx-auto flex justify-between items-center h-14">
+        <div>
+          <Link to="/" className="text-xl font-bold">
+            Lean Canvas
+          </Link>
+        </div>
+        <nav className="hidden md:flex space-x-4">
+          {navItems.map(item => (
+            <NavLink key={item.id} to={item.to} className="hover:text-gray-300">
+              {item.label}
+            </NavLink>
+          ))}
+        </nav>
+        <button className="md:hidden" onClick={toggleMenu}>
+          <FaBars />
+        </button>
+        <button className="hidden md:block bg-blue-500 hover:bg-blue-600 text-white font-bold py-1.5 px-4 rounded transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+          짐코딩 강의
+        </button>
+      </div>
+
+      {/* 모바일 메뉴 */}
+      <aside
+        className={`
+        fixed top-0 left-0 w-64 h-full bg-gray-800 z-50
+        ${isMenuOpen ? 'translate-x-0' : '-translate-x-full'}
+        md:hidden transform transition-transform duration-300 ease-in-out`}
+      >
+        <div className="flex justify-end p-4">
+          <button
+            className="text-white focus:outline-none"
+            aria-label="Close menu"
+            onClick={toggleMenu}
+          >
+            <FaTimes className="h-6 w-6" />
+          </button>
+        </div>
+        <nav className="flex flex-col space-y-4 p-4">
+          {navItems.map(item => (
+            <NavLink key={item.id} to={item.to} className="hover:text-gray-300">
+              {item.label}
+            </NavLink>
+          ))}
+        </nav>
+      </aside>
+    </header>
   );
 }
 
